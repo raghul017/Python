c94202c8fee33266656dcacd94927e97d19a0c82
Raghul.A.R <arraghul6@gmail.com>
2025-04-20 23:19:12 +0530

    Add operators.py with ternary operator example

---
+++ Day - 1/operators.py
@@ -0,0 +1,11 @@
+meaning = 42
+print("")
+
+# if meaning > 10:
+#     print("Right ON!")
+# else:
+#     print("Not Today")
+
+# Ternary Operators
+
+print("Right ON!") if meaning > 10 else print("Not Today")
